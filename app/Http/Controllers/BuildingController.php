c2be11686554adf0afaef0959db3dd8679ad5263
Ufaa <dhuwanee.no1@gmail.com>
2021-10-14 00:29:46 +0900

    first-commit

---
+++ app/Http/Controllers/BuildingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BuildingController extends Controller
+
+{
+    public function index($room = "建物です")
+    {
+        return $room;
+    }
+}
